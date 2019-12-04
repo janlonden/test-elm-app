e27b93fbdc4261d5eb2a41078e7713562d596e15
janlonden <jjlonden@gmail.com>
2019-12-04 22:33:39 +0100

    format

--- src/Main.elm
+++ src/Main.elm
@@ -1,52 +1,59 @@
 module Main exposing (main)
 
-import Browser
+import Browser exposing (Document)
 import Browser.Navigation as Nav
 import Html exposing (p, text)
 import Url
 
+
 main =
-  Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlRequest = LinkClicked
-    , onUrlChange = UrlChanged
-    }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = LinkClicked
+        , onUrlChange = UrlChanged
+        }
+
 
 type alias Model =
-  { key : Nav.Key
-  , arst : String
-  }
+    { key : Nav.Key
+    , arst : String
+    }
 
-type Msg =
-  NoOp
-  | LinkClicked Browser.UrlRequest
-  | UrlChanged Url.Url
 
-init : flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-  let
-    model =
-      { key = key
-      , arst = "arst" }
-  in
+type Msg
+    = NoOp
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    let
+        model =
+            { key = key
+            , arst = "arst"
+            }
+    in
     ( model, Cmd.none )
 
-view : Model -> Browser.Document Msg
-view { arst } =
-  { title = "yes"
-  , body =
-      [ p [] [ text arst ]
-      ]
-  }
 
-update : Msg -> Model -> ( Model, Cmd Msg)
+view : Model -> Document Msg
+view { arst } =
+    { title = "yes"
+    , body =
+        [ p [] [ text arst ]
+        ]
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  ( model, Cmd.none )
+    ( model, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
-
+    Sub.none
